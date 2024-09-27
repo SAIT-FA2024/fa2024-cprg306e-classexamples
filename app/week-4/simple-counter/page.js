fbb16b19c30719fa2280e108a88c1360cea1b4bd
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-09-27 13:49:49 -0600

    Week 4 Content

---
+++ app/week-4/simple-counter/page.js
@@ -0,0 +1,10 @@
+import SimpleCounter from "./counter";
+
+export default function SimpleCounterPage(){
+
+    return(
+        <main className="p-5">
+            <SimpleCounter />
+        </main>
+    );
+}
